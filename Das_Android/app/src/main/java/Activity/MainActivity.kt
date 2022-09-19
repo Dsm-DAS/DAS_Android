c656fe0937d8bdc4af5a795a0de7b3c30aac64f3
yeon <yeon8747@naver.com>
2022-09-19 23:35:17 +0900

    🌧 :: Main publishing

--- Das_Android/app/src/main/java/Activity/MainActivity.kt
+++ Das_Android/app/src/main/java/Activity/MainActivity.kt
@@ -1,15 +1,21 @@
 package Activity
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import org.techtown.das_android.R
+import org.techtown.das_android.databinding.ActivityMainBinding
 
-class MainActivity {
+class MainActivity : AppCompatActivity() {
 
-    class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-        }
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
