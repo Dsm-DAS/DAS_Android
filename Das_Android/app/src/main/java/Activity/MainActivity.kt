940c25978fdcad127b7bd5b5e2bbef803336bcf0
kim euno <eunoh@dsm.hs.kr>
2022-09-28 15:38:28 +0900

    Auto stash before merge of "main" and "origin/main"

--- Das_Android/app/src/main/java/Activity/MainActivity.kt
+++ Das_Android/app/src/main/java/Activity/MainActivity.kt
@@ -20,8 +20,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         navigationItemSelect()
-        intent = Intent(this,SignupActivity::class.java)
-        startActivity(intent)
+        //intent = Intent(this,SignupActivity::class.java)
+        //startActivity(intent)
     }
 
 
