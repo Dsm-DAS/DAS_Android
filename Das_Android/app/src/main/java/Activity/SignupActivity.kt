a308d9352f48956e8be8b014ab12477a212c8fff
JunsuMacbook <krpjs0508@gmail.com>
2022-10-07 06:56:27 +0900

    feat :: signUpActivity binding

--- Das_Android/app/src/main/java/Activity/SignupActivity.kt
+++ Das_Android/app/src/main/java/Activity/SignupActivity.kt
@@ -1,58 +1,46 @@
 package Activity
 
 import Api.ApiProvider
+import Base.BaseActivity
 import Request.SignupRequest
 import android.content.Intent
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.ImageView
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
 import org.techtown.das_android.R
 import org.techtown.das_android.databinding.ActivitySignupBinding
 import retrofit2.Call
 import retrofit2.Response
 
-class SignupActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySignupBinding
-    private var _binding:ActivitySignupBinding? = null
+class SignupActivity : BaseActivity<ActivitySignupBinding>(
+    R.layout.activity_signup
+) {
     companion object {
-        var DSMClass : String = ""
-        var DSMGrade : String = ""
+        var DSMClass: String = ""
+        var DSMGrade: String = ""
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        if(binding.ivSignupEmailprecautions.getVisibility() == View.INVISIBLE){
-//                binding.textView4.setVisibility(View.VISIBLE)
-//        }
-//
-
-
-        binding.ivSignupPwprecautions.setOnClickListener{
-            if (binding.textView5.getVisibility() == View.INVISIBLE){
+        binding.ivSignupPwprecautions.setOnClickListener {
+            if (binding.textView5.getVisibility() == View.INVISIBLE) {
                 binding.textView5.setVisibility(View.VISIBLE)
-            }
-            else{
+            } else {
                 binding.textView5.setVisibility(View.INVISIBLE)
             }
         }
 
         binding.ivSignupEmailprecautions.setOnClickListener {
-            if (binding.textView4.getVisibility() == View.INVISIBLE){
-            binding.textView4.setVisibility(View.VISIBLE)
-            }
-            else
-            {
+            if (binding.textView4.getVisibility() == View.INVISIBLE) {
+                binding.textView4.setVisibility(View.VISIBLE)
+            } else {
                 binding.textView4.setVisibility(View.INVISIBLE)
             }
         }
@@ -63,11 +51,11 @@ class SignupActivity : AppCompatActivity() {
         setupSpinnerHandler()       // 번호
 
 
-        binding.tvSignup.setOnClickListener{
+        binding.tvSignup.setOnClickListener {
             SignupCheck()               // 회원가입 글자 체크
         }
 
-        binding.back.setOnClickListener{
+        binding.back.setOnClickListener {
             finish()                // 뒤로가기
         }
 
@@ -78,10 +66,9 @@ class SignupActivity : AppCompatActivity() {
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 val length = binding.etSignupEmail.text.length
-                if (length >=  1){
+                if (length >= 1) {
                     binding.tvSignupEmailCheck.setBackgroundColor(Color.parseColor("#FB5B93"))
-                }
-                else {
+                } else {
                     binding.tvSignupEmailCheck.setBackgroundColor(Color.parseColor("#FFCCDE"))
                 }
             }
@@ -95,21 +82,17 @@ class SignupActivity : AppCompatActivity() {
     }
 
 
-
-
-    private fun SignupCheck(){
+    private fun SignupCheck() {
         val email = binding.etSignupEmail.text.length
-        val name =  binding.etSignupName.text.length
+        val name = binding.etSignupName.text.length
         val password = binding.etSignupPw.text.length
 
-        if (email == 0 && name == 0 && password == 0){
-            Toast.makeText(applicationContext,"모든 항목을 입력해주세요", Toast.LENGTH_SHORT).show()
-        }
-        else{
+        if (email == 0 && name == 0 && password == 0) {
+            Toast.makeText(applicationContext, "모든 항목을 입력해주세요", Toast.LENGTH_SHORT).show()
+        } else {
             Signup()
         }
     }
-
 
 
     private fun Signup() {
@@ -120,7 +103,7 @@ class SignupActivity : AppCompatActivity() {
         val classNum = DSMClass.toString().toInt()
         val number: Int = binding.etSignupNumber.toString().toInt()
 
-        val signupRequest = SignupRequest(email,password, name, grade, classNum, number)
+        val signupRequest = SignupRequest(email, password, name, grade, classNum, number)
 
         ApiProvider.retrofit.signup(signupRequest).enqueue(object : retrofit2.Callback<Void> {
             override fun onResponse(call: Call<Void>, response: Response<Void>) {
@@ -137,7 +120,7 @@ class SignupActivity : AppCompatActivity() {
             }
 
             override fun onFailure(call: Call<Void>, t: Throwable) {
-                Toast.makeText(applicationContext,"서버 켜...", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "서버 켜...", Toast.LENGTH_SHORT).show()
 
             }
 
@@ -147,22 +130,20 @@ class SignupActivity : AppCompatActivity() {
     }
 
 
-
-
     private fun setupSpinnerGrade() {
         val grade = resources.getStringArray(R.array.grade)
-        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,grade)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, grade)
         binding.spinnerGrade.adapter = adapter
     }
 
     private fun setupSpinnerClass_Num() {
         val classes = resources.getStringArray(R.array.classes)
-        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,classes)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, classes)
         binding.spinnerClass.adapter = adapter
     }
 
-    private fun setupSpinnerHandler(){
-        binding.spinnerGrade.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+    private fun setupSpinnerHandler() {
+        binding.spinnerGrade.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
@@ -176,7 +157,7 @@ class SignupActivity : AppCompatActivity() {
 
             }
         }
-        binding.spinnerClass.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        binding.spinnerClass.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
@@ -184,7 +165,6 @@ class SignupActivity : AppCompatActivity() {
                 id: Long
             ) {
                 DSMClass = binding.spinnerClass.selectedItem.toString()
-//                DSMClass = Integer.parseInt(binding.spinnerGrade.selectedItem.toString())
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -192,9 +172,5 @@ class SignupActivity : AppCompatActivity() {
             }
 
         }
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
     }
 }
