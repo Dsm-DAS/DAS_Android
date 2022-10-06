1824f9f2b8f1df649e4e74a4a8c52898d06b25af
JunsuMacbook <krpjs0508@gmail.com>
2022-10-06 23:27:17 +0900

    feat :: login binding

--- Das_Android/app/src/main/java/Activity/LoginActivity.kt
+++ Das_Android/app/src/main/java/Activity/LoginActivity.kt
@@ -2,20 +2,19 @@ package Activity
 
 import Base.BaseActivity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
+import org.techtown.das_android.R
+import org.techtown.das_android.databinding.ActivityLoginBinding
 
-class LoginActivity : BaseActivity<>(
-
+class LoginActivity : BaseActivity<ActivityLoginBinding>(
+    R.layout.activity_login
 ) {
-    private val binding by viewModels<LoginActivity> {  }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
 
-        binding.tvGoSignup.setOnClickListener{
+        binding.tvLoginGoToSignUp.setOnClickListener {
             val intent = Intent(applicationContext, SignupActivity::class.java)
             startActivity(intent)
         }
